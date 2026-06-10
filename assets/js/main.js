27f23a69541ebdd2cc8b453a29fce311cd284174
narayaniharvest <narayaniharvest@gmail.com>
2026-06-10 12:16:22 -0400

    Updated image

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,17 +11,17 @@ if (yearSpan) {
 // ===============================
 
 // Create toggle button dynamically (keeps HTML clean)
-const nav = document.querySelector("nav");
-if (nav) {
-  const toggleBtn = document.createElement("div");
-  toggleBtn.classList.add("menu-toggle");
-  toggleBtn.innerHTML = "&#9776;"; // hamburger icon
-  nav.parentNode.insertBefore(toggleBtn, nav);
+// const nav = document.querySelector("nav");
+// if (nav) {
+//   const toggleBtn = document.createElement("div");
+//   toggleBtn.classList.add("menu-toggle");
+//   toggleBtn.innerHTML = "&#9776;"; // hamburger icon
+//   nav.parentNode.insertBefore(toggleBtn, nav);
 
-  toggleBtn.addEventListener("click", () => {
-    nav.classList.toggle("open");
-  });
-}
+//   toggleBtn.addEventListener("click", () => {
+//     nav.classList.toggle("open");
+//   });
+// }
 
 document.addEventListener("scroll", () => {
   document.querySelectorAll(".reveal").forEach(el => {
